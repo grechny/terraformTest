50b87b06297f0f20ea7a9ce2fc5efdab9ba48475
Aliaksandr Hrechny <fotonn.mail@gmail.com>
2024-04-23 18:03:12 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,37 +27,34 @@ resource "google_compute_instance_template" "test_instance_template" {
 
   network_interface {
     network = "default"
-    tier    = "PREMIUM"
   }
 
   # Replace placeholders below with actual values from your CLI example
-  boot_disk {
+  boot {
     initialize_params {
-      image = "projects/debian-cloud/global/images/debian-11"  # Replace with desired image
+      image = "projects/debian-cloud/global/images/debian-11"
       disk_size_gb = 10
     }
   }
 
   service_account {
     email = "831965780848-compute@developer.gserviceaccount.com"
+    scopes = var.scopes
   }
-
-  shielded_vtpm {
-    enable = true
-  }
-
-  shielded_integrity_monitoring {
-    enable = true
-  }
-
-  scopes = var.scopes
 }
 
 # Create a Managed Instance Group resource
-resource "google_compute_instance_group_managed" "manual_test" {
+resource "google_compute_instance_group_manager" "manual_test" {
   name                = "manual-test"
   base_instance_name  = "manual-test"
   instance_template   = google_compute_instance_template.test_instance_template.self_link
   zone                = var.zone
   target_size         = 1
+
+  metadata = {
+    items = {
+      enable-vtpm = "true"
+      enable-integrity-monitoring = "true"
+    }
+  }
 }
