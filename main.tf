f340816f5b821c6730977ca967f159eb16834b50
Aliaksandr Hrechny <fotonn.mail@gmail.com>
2024-05-02 16:17:30 +0200

    030

--- main.tf
+++ main.tf
@@ -1,6 +1,32 @@
+variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "zone" {
+  type = string
+}
+
+variable "scopes" {
+  type = set(string)
+}
+
+variable "api_key" {
+  type = string
+}
+
 module "main" {
   source        = "./modules/generic"
 
   name          = "test-main"
   machineType   = "e2-micro"
+
+  project_id    = var.project_id
+  region        = var.region
+  zone          = var.zone
+  scopes        = var.scopes
+  api_key       = api_key
 }
