8cbc29a3f61576bb0adb0798f5a310fbab2ee14b
Aliaksandr Hrechny <fotonn.mail@gmail.com>
2024-05-02 15:57:58 +0200

    some tests

--- main.tf
+++ main.tf
@@ -2,12 +2,5 @@ module "main" {
   source        = "./modules/generic"
 
   name          = "test-main"
-  machineType  = "e2-micro"
-}
-
-module "slave" {
-  source        = "./modules/generic"
-
-  name          = "test-slave"
-  machineType  = "e2-standard"
+  machineType   = "e2-micro"
 }
