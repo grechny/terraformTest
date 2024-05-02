f7c8595c8f0259967982fbc073353ad1df38f2d7
Aliaksandr Hrechny <fotonn.mail@gmail.com>
2024-05-02 16:05:31 +0200

    001

--- main.tf
+++ main.tf
@@ -1,13 +1,5 @@
-variable "name" {
-  type = string
-}
-
-variable "machineType" {
-  type = string
-}
-
 module "main" {
-  source        = "./modules/generic"
+  source        = "./modules/generic/main.tf"
 
   name          = "test-main"
   machineType   = "e2-micro"
