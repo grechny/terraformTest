dd0fb358b0341333838f9437b319121afd9abda8
Aliaksandr Hrechny <fotonn.mail@gmail.com>
2024-05-02 15:29:44 +0200

    Create variables.tf

---
+++ modules/generic/variables.tf
@@ -0,0 +1,20 @@
+variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "zone" {
+  type = string
+}
+
+variable "scopes" {
+  type = set(string)
+}
+
+variable "api_key" {
+  type = string
+}
+.
