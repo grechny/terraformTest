b89da0af90696edd7ee83ce5b0ac5ec028552569
Aliaksandr Hrechny <fotonn.mail@gmail.com>
2024-05-07 11:51:02 +0200

    078

--- modules/generic/main.tf
+++ modules/generic/main.tf
@@ -29,7 +29,7 @@ resource "google_compute_region_instance_template" "generic_instance_template" {
   }
 
   metadata = {
-    user-data = data.local_file.init_script.content
+    user-data = resource.local_file.init_script.content
   }
 
   lifecycle {
